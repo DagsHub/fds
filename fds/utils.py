9594529d11e5d366ef67f1ee2c892c26a9921ab8
mohithg <mohithgm@gmail.com>
2021-05-24 00:50:30 +0530

    Remove str2bool and replace with store_true

--- fds/utils.py
+++ fds/utils.py
@@ -9,17 +9,6 @@ import humanize
 from fds.logger import Logger
 
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
 def get_size_of_path(path: str) -> int:
     if os.path.isdir(path):
         return sum(p.stat().st_size for p in Path(path).rglob('*'))
