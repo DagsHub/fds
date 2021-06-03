887030e02613d94c337e8b7ad8c15791ac5a0826
mohithg <mohithgm@gmail.com>
2021-06-03 17:28:37 +0530

    Add corner caes for init

--- tests/it/test_fds.py
+++ tests/it/test_fds.py
@@ -12,4 +12,19 @@ class TestFds(IntegrationTestCase):
         assert does_file_exist(f"{self.repo_path}/.git")
         assert does_file_exist(f"{self.repo_path}/.dvc")
 
+    def test_init_git_exists(self):
+        os.chdir(self.repo_path)
+        execute_command(["git", "init"])
+        assert does_file_exist(f"{self.repo_path}/.git") is True
+        self.fds_service.init()
+        assert does_file_exist(f"{self.repo_path}/.git") is True
+        assert does_file_exist(f"{self.repo_path}/.dvc") is True
+
+    def test_init_dvc_exists(self):
+        os.chdir(self.repo_path)
+        execute_command(["dvc", "init", "--no-scm"])
+        assert does_file_exist(f"{self.repo_path}/.dvc") is True
+        self.fds_service.init()
+        assert does_file_exist(f"{self.repo_path}/.git") is True
+        assert does_file_exist(f"{self.repo_path}/.dvc") is True
 
