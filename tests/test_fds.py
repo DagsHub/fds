e147c64bfb20a611b1279d54f37a514558303de6
mohithg <mohithgm@gmail.com>
2021-06-13 13:30:31 +0530

    Add unit tests

--- tests/test_fds.py
+++ tests/test_fds.py
@@ -101,3 +101,25 @@ class TestFds(unittest.TestCase):
         self.assertRaises(Exception, mock_dvc_service.commit)
         assert mock_dvc_service.commit.called
         assert mock_git_service.commit.notcalled
+
+    @patch('fds.services.dvc_service.DVCService')
+    @patch('fds.services.git_service.GitService')
+    def test_clone_dvc_failure(self, mock_git_service, mock_dvc_service):
+        mock_dvc_service.pull.side_effect = Exception
+        fds_service = FdsService(mock_git_service, mock_dvc_service)
+        with self.assertRaises(Exception) as cm:
+            fds_service.clone("https://github.com/dagshub/fds.git")
+        self.assertRaises(Exception, mock_dvc_service.pull)
+        assert mock_git_service.clone.called
+        assert mock_dvc_service.pull.called
+
+    @patch('fds.services.dvc_service.DVCService')
+    @patch('fds.services.git_service.GitService')
+    def test_clone_git_failure(self, mock_git_service, mock_dvc_service):
+        mock_git_service.clone.side_effect = Exception
+        fds_service = FdsService(mock_git_service, mock_dvc_service)
+        with self.assertRaises(Exception) as cm:
+            fds_service.clone("https://github.com/dagshub/fds.git")
+        self.assertRaises(Exception, mock_git_service.clone)
+        assert mock_git_service.clone.called
+        assert mock_dvc_service.pull.notcalled
