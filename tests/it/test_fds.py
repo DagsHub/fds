f9c2f2aaf42f9b5327e6c40d4aa2dded161a14f3
mohithg <mohithgm@gmail.com>
2021-07-18 13:19:37 +0530

    Add fds test

--- tests/it/test_fds.py
+++ tests/it/test_fds.py
@@ -21,7 +21,7 @@ class TestFds(IntegrationTestCase):
         self.fds_service.init()
         super().create_fake_git_data()
         super().create_fake_dvc_data()
-        self.fds_service.add(".")
+        self.fds_service.add(["."])
         output = execute_command(["git", "status"], capture_output=True)
         # Check DVC add
         assert "new file:   large_file.dvc" in convert_bytes_to_string(output.stdout)
@@ -32,6 +32,16 @@ class TestFds(IntegrationTestCase):
         assert "new file:   git_data/file-3" in convert_bytes_to_string(output.stdout)
         assert "new file:   git_data/file-4" in convert_bytes_to_string(output.stdout)
 
+    @patch("fds.services.dvc_service.DVCService._get_choice", return_value={"selection_choice": DvcChoices.ADD_TO_DVC.value})
+    def test_add_multiple_paths(self, get_choice):
+        self.fds_service.init()
+        super().create_fake_git_data()
+        super().create_fake_dvc_data()
+        self.fds_service.add(["dvc_data"])
+        output = execute_command(["git", "status"], capture_output=True)
+        # Check untracked
+        assert "\n\tlarge_file" in convert_bytes_to_string(output.stdout)
+        assert "\n\tgit_data/" in convert_bytes_to_string(output.stdout)
 
     @patch("fds.services.dvc_service.DVCService._get_choice", return_value={"selection_choice": DvcChoices.IGNORE.value})
     def test_add_dvc_ignore(self, get_choice):
